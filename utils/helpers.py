0eea45d53d017e6eb893cebafb5cdbffb76314cb
issacj-17 <68261814+issacj-17@users.noreply.github.com>
2024-04-12 22:16:41 +0800

    Pushed notebooks to github to continue work

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,30 @@
-import pandas
+# import pandas
 
-# Saver for preprocessed dataframe
-def save_df(df, set):
-    df.to_csv(f'/work/preprocessed_{set}.csv', sep=',', encoding='utf-8', index=False)
+# # Saver for preprocessed dataframe
+# def save_df(df, set):
+#     df.to_csv(f'/work/preprocessed_{set}.csv', sep=',', encoding='utf-8', index=False)
 
-# Loader for preprocessed dataframe
-def load_df(path):
-    df = pandas.read_csv(path)
-    return df
+# # Loader for preprocessed dataframe
+# def load_df(path):
+#     df = pandas.read_csv(path)
+#     return df
+
+# Saver/loader
+
+import torch
+import os
+def save_model_ckpt(self, model, ):
+    print("saving model checkpoint, itr:", itr)
+    model_path = os.path.join(self.model_save_dir, '{}-.ckpt'.format(itr))
+
+    torch.save(model.state_dict(), model_path)
+    print('Saved model checkpoints into {}...'.format(self.model_save_dir))
+
+# Restore the model 
+# Note that itr is the value of the last checkpoint file
+# The new starting epoch is (itr + 1) 
+def restore_model(self, model, itr):
+    print('Restore the trained models')
+    model_path = os.path.join(self.model_save_dir, '{}-.ckpt'.format(itr))
+       
+    model.load_state_dict(torch.load(model_path))
