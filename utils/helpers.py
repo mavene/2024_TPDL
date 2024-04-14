9bcf2642ab868148bde789886c75a6d9868c7ada
issacj-17 <68261814+issacj-17@users.noreply.github.com>
2024-04-14 16:12:07 +0800

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,6 +3,8 @@ import json
 import os
 import numpy
 import random
+from copy import deepcopy
+import matplotlib.pyplot as plt
 
 # Saves the model
 def save_model(model, optimizer, epoch, path):
@@ -37,3 +39,20 @@ def load_model(model_constructor, modelName, epoch, path='../models'):
     model = load_config(model_constructor, modelName, path)
     model = restore_model(model, modelName, epoch, path)
     return model
+
+def plot_values(train_values, valid_values, metric, title):
+    plt.figure(figsize=(9, 6))
+    plt.plot(train_values, label='Train')
+    plt.plot(valid_values, label='Validation')
+    plt.title(title)
+    plt.xlabel('Epoch')
+    plt.ylabel(metric)
+    plt.legend()
+    plt.show()
+
+def detach_tensor(tensor):
+    return tensor.detach().cpu().numpy()
+
+def process_tensors(lst):
+    copy = deepcopy(lst)
+    return [detach_tensor(tensor) for tensor in copy]
