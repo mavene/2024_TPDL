ba5379886929229ed9e4e61ff61a0f917211fa23
mavene <dianmaisara@gmail.com>
2024-04-14 15:36:06 +0800

    Update helpers.py, Add preprocessors.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,39 +1,56 @@
-# import pandas
-
-# # Saver for preprocessed dataframe
-# def save_df(df, set):
-#     df.to_csv(f'/work/preprocessed_{set}.csv', sep=',', encoding='utf-8', index=False)
-
-# # Loader for preprocessed dataframe
-# def load_df(path):
-#     df = pandas.read_csv(path)
-#     return df
-
-# Saver/loader
-
 import torch
+import json
 import os
+import pandas
+import numpy
+import random
 
-# Saves the model
-def save_model_ckpt(self, model, epoch, optimizer, loss):
-    print("Saving model checkpoint on epoch %d:", epoch)
-    model_path = os.path.join(self.model_save_dir, '{}-.ckpt'.format(epoch))
+from torch.utils.data import Dataset
 
-    torch.save({
-        'epoch' : epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'loss': loss.item()
-    }, model_path)
+import torchvision.transforms as T
+from torchvision.transforms import v2
+from torchvision.io import read_image, ImageReadMode
 
-# Restore the model
-def restore_model(self, model, epoch, optimizer, loss):
-    print('Restore the trained models')
-    model_path = os.path.join(self.model_save_dir, '{}-.ckpt'.format(epoch))
-       
+# Seeder for reproducibility
+def seed_everything(seed: int):
+    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    numpy.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    
+# Implementation of Custom Dataset Class for CheXPhoto Dataset
+class CheXDataset(Dataset):
+    # Accepts dataframe object and str
+    def __init__(self, df: pandas.DataFrame, px_size: int = 256):
+        self.dataframe = df.copy()
+        self.px_size = px_size
+        self.transform = T.Compose([
+            v2.Resize((self.px_size, self.px_size), interpolation=T.InterpolationMode.BICUBIC)
+        ])
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, idx):
+        x_path = DATA_PATH + "/" + self.dataframe.iloc[idx, 0].split("CheXphoto-v1.0", 1)[-1]
+        resized_x_tensor = self.transform(read_image(x_path, mode = ImageReadMode.RGB)) /255
+        y = torch.tensor(self.dataframe.iloc[idx, 1]).type(torch.LongTensor)
+        return resized_x_tensor, y
+
+# Saver / loader - model
+def load_config(model_constructor, model_name, path='../models'):
+    config_path = path + "/" + model_name + "/" + "config.json"
+    with open(config_path, "r") as f:
+        args = json.load(f)
+        return model_constructor(**args)
+    
+def restore_model(model, model_name, epoch, path='../models'):
+    model_path = path + "/" + model_name + "/" + f'{epoch}.pt'
     model.load_state_dict(torch.load(model_path)['model_state_dict'])
-    #optimizer.load_state_dict(torch.load(model_path)['optimizer_state_dict'])
-    #loss.load_state_dict(torch.load(model_path)['loss'])
-    #loss.load_state_dict(torch.load(model_path)['epoch'])
+    return model
 
-    model.eval()
+def load_model(model_constructor, modelName, epoch, path='../models'):
+    model = load_config(model_constructor, modelName, path)
+    model = restore_model(model, modelName, epoch, path)
